92cc1c6065e61923d5afa34c7bde7051a3a3d206
djumanov <diyorbekjumanov1120@gmail.com>
2022-08-13 17:16:40 +0500

    last msg's chat id and text

--- get_random_user.py
+++ get_random_user.py
@@ -1,15 +1,25 @@
 import requests
 
 
-token = '5528595058:AAHoj8gqsXVAR91sWHa2CocMU8qydTTh_Oo'
+token = '2057309601:AAFJaNwnVt3Wrz2dAP98VRxIwp3VDxhLalU'
 
 
 def getUpdates():
     r = requests.get(url=f'https://api.telegram.org/bot{token}/getUpdates')
     if r.status_code == 200:
-        results = r.json()['result']
+        results      = r.json()['result']
         last_message = results[-1]
-        print(last_message)
-        return last_message
+        update_id    = last_message['update_id']
+        msg = last_message['message']
+        print(msg)
+        chat_id      = msg['from']['id']
+        text         = msg.get('text', 'no')
 
-getUpdates()
+        return update_id, chat_id, text
+    
+    return -1, None, None
+
+print(getUpdates())
+
+
+
